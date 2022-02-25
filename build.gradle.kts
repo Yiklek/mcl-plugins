807d74e955c489212f558024913ac92b60503b93
Yiklek <yiklek.me@gmail.com>
2022-02-25 13:12:15 +0800

    refactor(all): shadowJar

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,8 +2,8 @@ plugins {
     val kotlinVersion = "1.6.10"
     kotlin("jvm") version kotlinVersion
     kotlin("plugin.serialization") version kotlinVersion
-
-    id("net.mamoe.mirai-console") version "2.10.0"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
+    application
 }
 
 group = "io.github.yiklek"
@@ -13,16 +13,39 @@ repositories {
     maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
     mavenCentral()
 }
-subprojects {
-    apply(plugin = "net.mamoe.mirai-console")
+allprojects {
     apply(plugin = "kotlin")
     apply(plugin = "org.jetbrains.kotlin.plugin.serialization")
     repositories {
         mavenCentral()
         maven("https://maven.aliyun.com/repository/public") // 阿里云国内代理仓库
     }
+    dependencies {
+        api(platform("net.mamoe:mirai-bom:2.10.0"))
+        implementation("net.mamoe:mirai-console-terminal")
+        implementation("net.mamoe:mirai-console")
+        implementation("net.mamoe:mirai-core")
+        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+    }
 }
 dependencies {
-    testImplementation(project(":reply-trigger"))
-    testImplementation(project(":cloud-recruit"))
+    implementation(project(":reply-trigger"))
+    implementation(project(":cloud-recruit"))
+    implementation("xyz.cssxsh.mirai:mirai-device-generator:1.0.2")
+}
+application {
+    mainClass.set("RunMiraiKt")
+    mainClassName = mainClass.get()
+}
+
+tasks {
+    shadowJar {
+        mergeServiceFiles()
+    }
+    build {
+        dependsOn(shadowJar)
+    }
+}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions.jvmTarget = "11"
 }
