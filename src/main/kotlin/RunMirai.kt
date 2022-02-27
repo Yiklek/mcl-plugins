b110ed5cb79d4b76c8ff49e005b0b0261453f1d9
Yiklek <yiklek.me@gmail.com>
2022-02-27 15:29:34 +0800

    refactor:  load plugins

--- src/main/kotlin/RunMirai.kt
+++ src/main/kotlin/RunMirai.kt
@@ -3,18 +3,24 @@ import com.github.yiklek.mcl.plugin.trigger.ReplyTriggerPlugin
 import net.mamoe.mirai.console.MiraiConsole
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.enable
 import net.mamoe.mirai.console.plugin.PluginManager.INSTANCE.load
+import net.mamoe.mirai.console.plugin.jvm.AbstractJvmPlugin
 import net.mamoe.mirai.console.plugins.chat.command.PluginMain
 import net.mamoe.mirai.console.terminal.MiraiConsoleTerminalLoader
 import net.mamoe.mirai.console.util.ConsoleExperimentalApi
+import xyz.cssxsh.mirai.plugin.MiraiDevicePlugin
 
 @ConsoleExperimentalApi
 suspend fun main() {
     MiraiConsoleTerminalLoader.startAsDaemon()
-    PluginMain.load()
-    PluginMain.enable()
-    ReplyTriggerPlugin.load()
-    ReplyTriggerPlugin.enable()
-    CloudRecruitPlugin.load()
-    CloudRecruitPlugin.enable()
+    val pluginList = listOf<AbstractJvmPlugin>(
+        PluginMain,
+        ReplyTriggerPlugin,
+        CloudRecruitPlugin,
+        MiraiDevicePlugin
+    )
+    pluginList.forEach {
+        it.load()
+        it.enable()
+    }
     MiraiConsole.job.join()
 }
